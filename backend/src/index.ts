d6eb0ed113dfed299e70f1ac25f78d3f892504bf
Prashnt Sachan <swepra04@gmail.com>
2024-04-07 16:06:10 +0530

    login auth added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,9 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users";
-import authRoutes from "./routes/auth"
+import authRoutes from "./routes/auth";
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
@@ -11,9 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-
-app.use("/api/auth",authRoutes);
-app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes); 
 
 app.listen(7000, () => {
   console.log("server is running on localhost :7000");
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -41,7 +41,7 @@ router.post(
         secure: process.env.NODE_ENV === "production",
         maxAge: 86400000,
       });
-      return res.sendStatus(200);
+      return res.status(200).send({ message: "User registered OK" });
     } catch (error) {
       console.log(error);
       res.status(500).send({ message: "Something went wrong" });
@@ -50,5 +50,3 @@ router.post(
 );
 
 export default router;
-
-
